9a72c436e770f41d846f1f5f4a391a4f7c9e0234
biancajj1 <biancajj1@gmail.com>
2022-10-22 00:40:58 -0600

    challenge 16

--- app.js
+++ app.js
@@ -10,14 +10,39 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+let posts=[];
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-
 app.get("/", function(req,res){
-  res.render("home");
+  res.render("home", {startingContent : homeStartingContent,
+posts: posts
+  });
+});
+  app.get("/about", function(req,res){
+    res.render("about", {aboutContent : aboutContent});
+});
+    app.get("/contact", function(req,res){
+      res.render("contact", { contactContent : contactContent});
+});
+
+app.get("/compose", function(req,res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req,res){
+  const post= {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+
+  posts.push(post);
+
+  res.redirect("/");
+
 });
 app.listen(3000, function() {
   console.log("Server started on port 3000");
